865efbb61b6d46cf33b2d7cf63a5ee20ea5090cc
Eve <hi.its.eve@gmail.com>
2022-03-03 21:58:53 -0500

    clean up some old taiko calc code

--- src/gameplay/modes/taiko/diff_calc/calculator.rs
+++ src/gameplay/modes/taiko/diff_calc/calculator.rs
@@ -1,13 +1,8 @@
-use glfw_window::GlfwWindow;
+
 use prelude::taiko::TaikoGame;
 
 use crate::prelude::*;
-use super::difficulty_hit_object::{DifficultyHitObject, DECAY_BASE};
-
-// constants
-const STAR_SCALING_FACTOR:f64 = 0.04125;
-const STRAIN_STEP:f64 = 400.0;
-const DECAY_WEIGHT:f64 = 0.9;
+use super::difficulty_hit_object::DifficultyHitObject;
 
 // how long each "group" of notes is
 const BUCKET_LENGTH:f32 = 500.0;
@@ -24,104 +19,6 @@ pub struct TaikoDifficultyCalculator {
     difficulty_hitobjects: Vec<DifficultyHitObject>
 }
 impl TaikoDifficultyCalculator {
-    // pub fn compute_difficulty(&mut self) -> f64 {
-    //     if !self.calc_strain_values() {
-    //         return 0.0;
-    //     };
-        
-    //     let star_rating = self.calculate_difficulty() * STAR_SCALING_FACTOR;
-
-    //     // if (CategoryDifficulty != null) {
-    //     //     CategoryDifficulty.Add("Strain", StarRating.ToString("0.00", GameBase.nfi));
-    //     //     CategoryDifficulty.Add("Hit window 300", (this.HitObjectManager.HitWindow300_noSlider / this.TimeRate_noSlider).ToString("0.00", GameBase.nfi));
-    //     // }
-
-    //     star_rating
-    // }
-
-    // fn calc_strain_values(&mut self) -> bool {
-    //     let mut enumerator = self.difficulty_hitobjects.iter_mut();
-
-    //     let x = enumerator.next();
-    //     if let None = x {
-    //         println!("bad");
-    //         return false;
-    //     }
-    //     let mut previous = x.unwrap();
-
-    //     while let Some(current) = enumerator.next() {
-    //         // println!("calc!");
-    //         current.calculate_strains(&previous, self.time_rate);
-    //         previous = current;
-    //     }
-    //     true
-        
-    //     // // Traverse hitObjects in pairs to calculate the strain value of NextHitObject from the strain value of CurrentHitObject and environment.
-    //     // List<DifficultyHitObjectTaiko>.Enumerator HitObjectsEnumerator = this.DifficultyHitObjects.GetEnumerator();
-    //     // if (HitObjectsEnumerator.MoveNext() == false) return false;
-
-    //     // DifficultyHitObjectTaiko CurrentHitObject = HitObjectsEnumerator.Current;
-    //     // DifficultyHitObjectTaiko NextHitObject;
-
-    //     // // First hitObject starts at strain 1. 1 is the default for strain values, so we don't need to set it here. See DifficultyHitObject.
-
-    //     // while (HitObjectsEnumerator.MoveNext()) {
-    //     //     NextHitObject = HitObjectsEnumerator.Current;
-    //     //     NextHitObject.CalculateStrains(CurrentHitObject, this.TimeRate_noSlider);
-    //     //     CurrentHitObject = NextHitObject;
-    //     // }
-
-    //     // return true;
-    // }
-    // fn calculate_difficulty(&mut self) -> f64 {
-    //     let actual_strain_step = STRAIN_STEP * self.time_rate;
-
-    //     // Find the highest strain value within each strain step
-    //     let mut highest_strains = Vec::new();
-    //     let mut interval_end_time = actual_strain_step;
-    //     let mut maximum_strain = 0.0; // We need to keep track of the maximum strain in the current interval
-
-    //     let iter = self.difficulty_hitobjects.iter_mut();
-    //     let mut previous:Option<DifficultyHitObject> = None;
-
-    //     for hitobject in iter {
-    //         // While we are beyond the current interval push the currently available maximum to our strain list
-    //         while hitobject.time > interval_end_time {
-    //             highest_strains.push(maximum_strain);
-
-    //             // The maximum strain of the next interval is not zero by default! We need to take the last hitObject we encountered, take its strain and apply the decay
-    //             // until the beginning of the next interval.
-    //             if let Some(previous) = &previous {
-    //                 let decay = DECAY_BASE.powf(interval_end_time - previous.time as f64 / 1000.0);
-    //                 maximum_strain = previous.strain * decay;
-    //             } else {
-    //                 maximum_strain = 0.0;
-    //             }
-
-    //             // Go to the next time interval
-    //             interval_end_time += actual_strain_step;
-    //         }
-
-    //         // Obtain maximum strain
-    //         if hitobject.strain > maximum_strain {
-    //             maximum_strain = hitobject.strain;
-    //         }
-
-    //         previous = Some(hitobject.clone());
-    //     }
-
-    //     // Build the weighted sum over the highest strains for each interval
-    //     let mut difficulty = 0.0;
-    //     let mut weight = 1.0;
-    //     highest_strains.sort_by(|a, b| b.partial_cmp(a).unwrap()); // Sort from highest to lowest strain.
-
-    //     for strain in highest_strains {
-    //         difficulty += weight * strain;
-    //         weight *= DECAY_WEIGHT;
-    //     }
-
-    //     difficulty
-    // }
 
     fn note_density(&mut self) -> TatakuResult<Vec<usize>> {
         let mut start_bucket_time = self.difficulty_hitobjects.first().unwrap().time;
@@ -265,7 +162,7 @@ impl DiffCalc<TaikoGame> for TaikoDifficultyCalculator {
                 continue;
             }
 
-            println!("strain: {}, density: {}, strain value: {}, density value: {}, combined: {}", strain, density, strain_value, density_value, combined);
+            // println!("strain: {}, density: {}, strain value: {}, density value: {}, combined: {}", strain, density, strain_value, density_value, combined);
         }
         
         let mean = total / count;
@@ -307,20 +204,24 @@ fn try_calc(path: impl AsRef<Path>) -> TatakuResult<()> {
     // load map
     let beatmap = Beatmap::load(path)?;
     if beatmap.playmode(PlayMode::Standard) != PlayMode::Taiko {return Ok(())}
+
     
     println!("--- trying map: {}", beatmap.get_beatmap_meta().version_string());
+    let mut benchmark = BenchmarkHelper::new("a");
     let mode = TaikoGame::new(&beatmap)?;
 
     // test calc
     let mut calc = TaikoDifficultyCalculator::new(&mode)?;
     let diff = calc.calc()?;
     println!("got diff: {}", diff);
+    benchmark.log("done", true);
 
     Ok(())
 }
 
 #[test]
 fn taiko_calc_test() -> TatakuResult<()> {
+    use glfw_window::GlfwWindow;
 
     let window: GlfwWindow = piston::WindowSettings::new("Tataku!", [10, 10])
         .graphics_api(opengl_graphics::OpenGL::V3_2)
