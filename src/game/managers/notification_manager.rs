6f50b89896ca8c5b9e7cd0cecce7bf3c9ce70121
Eve <mattinator123@gmail.com>
2022-03-13 18:50:27 -0400

    add border rounding and padding to notifications

--- src/game/managers/notification_manager.rs
+++ src/game/managers/notification_manager.rs
@@ -1,12 +1,23 @@
 use crate::prelude::*;
 
 
-const NOTIF_WIDTH:f64 = 300.0;
+const NOTIF_WIDTH:f64 = 300.0; // TODO: have this as the max width instead
 const NOTIF_Y_OFFSET:f64 = 100.0; // window_size().y - this
 const NOTIF_TEXT_SIZE:u32 = 15;
 const NOTIF_DEPTH:f64 = -800_000_000.0;
 const NOTIF_TEXT_HEIGHT:f64 = 20.0;
+
+/// how many pixels of space should there be between notifications?
 const NOTIF_Y_MARGIN:f64 = 5.0;
+
+/// how rounded the borders are
+const NOTIF_BORDER_ROUNDING:f64 = 5.0;
+
+/// how many pixels of padding should the notif text have?
+const NOTIF_PADDING:Vector2 = Vector2::new(2.0, 2.0);
+
+/// what background color should the notifs have?
+const NOTIF_BG_COLOR:Color = Color::new(0.0, 0.0, 0.0, 0.6);
 
 
 lazy_static::lazy_static! {
@@ -25,7 +36,7 @@ impl NotificationManager { // static
     }
     pub fn add_text_notification(text: &str, duration: f32, color: Color) {
         let notif = Notification::new(text.to_owned(), color, duration, NotificationOnClick::None);
-        
+
         println!("adding text notif");
         Self::add_notification(notif);
     }
@@ -33,7 +44,7 @@ impl NotificationManager { // static
         let error:TatakuError = error.into();
         let text = format!("{}:\n{}", msg, error);
         
-        println!("{}", text);
+        // println!("{}", text);
         Self::add_text_notification(
             &text, 
             5_000.0, 
@@ -51,7 +62,7 @@ impl NotificationManager { // non-static
 
     pub fn update(&mut self) {
         for notif in std::mem::take(&mut self.pending_notifs) {
-            println!("adding notif");
+            // println!("adding notif");
             let new = ProcessedNotif::new(notif);
 
             // move all the other notifs up
@@ -146,19 +157,18 @@ impl ProcessedNotif {
 
         let mut lines = Vec::new();
         let split = notification.text.split('\n').collect::<Vec<&str>>();
-        for i in 0..split.len() {
-            let i = (split.len() - 1) - i; // reverse
+        for i in (0..split.len()).rev() {
             lines.push(Text::new(
                 Color::WHITE,
                 NOTIF_DEPTH - 0.1,
-                Vector2::new(2.0, NOTIF_TEXT_HEIGHT * i as f64),
+                NOTIF_PADDING + Vector2::new(2.0, NOTIF_TEXT_HEIGHT * i as f64),
                 NOTIF_TEXT_SIZE,
                 split[i].to_owned(),
                 font.clone()
             ))
         }
 
-        let size = Vector2::new(NOTIF_WIDTH, NOTIF_TEXT_HEIGHT * lines.len() as f64);
+        let size = NOTIF_PADDING * 2.0 + Vector2::new(NOTIF_WIDTH, NOTIF_TEXT_HEIGHT * lines.len() as f64);
         let pos = Vector2::new(window_size.x - NOTIF_WIDTH, window_size.y - (NOTIF_Y_OFFSET + size.y));
 
         Self {
@@ -180,7 +190,7 @@ impl ProcessedNotif {
     fn draw(&self, list: &mut Vec<Box<dyn Renderable>>) {
         // bg
         list.push(Box::new(Rectangle::new(
-            Color::new(0.0, 0.0, 0.0, 0.6),
+            NOTIF_BG_COLOR,
             NOTIF_DEPTH + 0.1,
             self.pos,
             self.size,
@@ -188,7 +198,7 @@ impl ProcessedNotif {
                 self.notification.color,
                 1.2
             ))
-        )));
+        ).shape(Shape::Round(NOTIF_BORDER_ROUNDING, 10))));
 
         for (i, text) in self.lines.iter().rev().enumerate() {
             let mut text = text.clone();
@@ -197,8 +207,8 @@ impl ProcessedNotif {
                 0.0,
                 NOTIF_TEXT_HEIGHT * i as f64, 
                 // (self.size.y - (text.font_size as f64) * i as f64) / 2.0
-            );
-            text.initial_pos = txt_pos;
+            ) + NOTIF_PADDING;
+            // text.initial_pos = txt_pos;
             text.current_pos = txt_pos;
             list.push(Box::new(text));
         }
