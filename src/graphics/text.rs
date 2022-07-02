55a2c0b0e973db9297232872d076f2c3067327f9
Eve <mattinator123@gmail.com>
2022-07-02 00:25:50 -0400

    remove that temp thing i was messing with oops

--- src/graphics/text.rs
+++ src/graphics/text.rs
@@ -69,12 +69,6 @@ impl Text {
         self.initial_pos = rect.current_pos + (rect.size - text_size)/2.0; // + Vector2::new(0.0, text_size.y);
         self.current_pos = self.initial_pos;
     }
-    pub fn center_text_chain(mut self, rect:[Vector2;2]) -> Self {
-        let text_size = self.measure_text();
-        self.initial_pos = rect[0] + (rect[1] - text_size)/2.0; // + Vector2::new(0.0, text_size.y);
-        self.current_pos = self.initial_pos;
-        self
-    }
 
     pub fn _set_text_colors(&mut self, colors: Vec<Color>) {
         self.text_colors = colors
