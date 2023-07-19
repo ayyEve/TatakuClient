b0c8f79675c863a54bc9ec27204f42f3379430a1
Eve <hi.its.eve@gmail.com>
2023-07-19 06:50:03 -0400

    remove font loader because i didnt end up actually creating it

--- src/engine/graphics/drawables/font/mod.rs
+++ src/engine/graphics/drawables/font/mod.rs
@@ -1,11 +1,9 @@
 mod font;
 mod font_size;
 mod char_data;
-mod font_loader;
 mod font_awesome;
 
 pub use font::*;
 pub use font_size::*;
 pub use char_data::*;
-pub use font_loader::*;
 pub use font_awesome::*;
